974e339ac847a6dddbd035fdf5f14065a179c5da
IrynaZubarieva <iphonezub1@gmail.com>
2022-01-20 11:38:57 +0200

    homework 6 fin

---
+++ js/task-07.js
@@ -0,0 +1,15 @@
+const fontSizeControl = document.querySelector("input");
+const text = document.querySelector("span");
+text.style.fontSize = fontSizeControl.value + "px";
+//console.log(fontSizeControl);
+fontSizeControl.addEventListener("input", sizeControlHandler);
+
+function sizeControlHandler(event) {
+    text.style.fontSize = event.currentTarget.value + "px";
+}
+
+// Напиши скрипт, который реагирует на изменение значения 
+// input#font-size-control (событие input) и изменяет инлайн-стиль span#text 
+// обновляя свойство font-size.
+// В результате при перетаскивании ползунка будет меняться размер текста.
+
--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,26 @@
+const formRef = document.querySelector(".login-form");
+const inputRef = document.querySelector("input");
+console.log()
+
+formRef.addEventListener("submit", formSubmitHandler);
+
+function formSubmitHandler(event){
+
+    event.preventDefault();
+
+    if (formRef.elements.password.value !== "" && formRef.elements.email.value !== "") {
+        const email = formRef.elements.email.value;
+        const password = formRef.elements.password.value;
+        const formData = { email, password, };
+        formRef.reset();
+        return console.log(formData);
+    }
+    
+    else {
+        formRef.reset();
+        return alert("все поля должны быть заполнены");
+
+    }
+}
+
 
